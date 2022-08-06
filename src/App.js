0807603e5f6a1e74561fa699aff2c77e2866f2ca
monikkacha <monikkacha@gmail.com>
2022-08-06 17:57:54 +0530

    color changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import Card from './component/card';
+import { useEffect } from 'react';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
+
+  // useEffect(() => findTheAccount(), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Card child={"Didn't find any account ,create new one "} btnLabel={"Start"} />
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path='/' element={<Card child={"Didn't find any account ,create new one "} btnLabel={"next"} />}>
+              <Route path='create' element={<Card child={"Fuck you ,create new one "} btnLabel={"next"} />}></Route>
+            </Route>
+          </Routes>
+        </header >
+      </div >
+    </BrowserRouter>
   );
+
+
 }
 
 export default App;
